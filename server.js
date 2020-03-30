57669888fc3780af9f43c3ff21852299d58408c2
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2020-03-30 10:38:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
-var Array=[];
-app.get('/add',function(req,res){
-    
-    Array.push(req.body.todo);
-    for (var i = 0; i < Array.length; i++) {
-        res.send(Array[i]+"<br>");
-    }
-      
-});
-
 var server = app.listen(9005, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
